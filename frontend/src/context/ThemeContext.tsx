bc50cfed935b2f8719a56047560a460b88fa18bc
az01r <paoloantonio.grosso@gmail.com>
2025-12-05 08:39:01 +0100

    Removed unused import

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import type { ReactNode } from 'react';
 
 type Theme = 'light' | 'dark';
